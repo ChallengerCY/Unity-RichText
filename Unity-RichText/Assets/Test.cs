3356a433ce3ac642ea03a32f856328315d090e40
ChallengerCY <790597004@qq.com>
2018-07-07 21:31:54 +0800

    modify

---
+++ Unity-RichText/Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //private void OnGUI()
+    //{
+    //    GUIStyle style = new GUIStyle();
+    //    style.richText = true;
+    //    GUILayout.Label("GUI RichText", style);
+    //    GUILayout.Label("<size=30>Some <color=yellow>RICH</color> text</size>", style);
+    //}
+}
